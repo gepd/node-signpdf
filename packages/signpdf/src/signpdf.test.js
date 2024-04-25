cdfe61bbf9937de9aececca7aa91f02c3753f574
gepd <guillermoepd@hotmail.com>
2024-04-25 13:47:17 -0400

    update tests

--- packages/signpdf/src/signpdf.test.js
+++ packages/signpdf/src/signpdf.test.js
@@ -5,25 +5,6 @@ import {
 import signpdf from './signpdf';
 
 describe('Test signing', () => {
-    it('expects P12 signer to be Buffer', async () => {
-        try {
-            await signpdf.sign(Buffer.from(''), 'non-buffer');
-            expect('here').not.toBe('here');
-        } catch (e) {
-            expect(e instanceof SignPdfError).toBe(true);
-            expect(e.type).toBe(SignPdfError.TYPE_INPUT);
-            expect(e.message).toMatchInlineSnapshot('"Signer implementation expected."');
-        }
-
-        try {
-            await signpdf.sign(Buffer.from(''), {});
-            expect('here').not.toBe('here');
-        } catch (e) {
-            expect(e instanceof SignPdfError).toBe(true);
-            expect(e.type).toBe(SignPdfError.TYPE_INPUT);
-            expect(e.message).toMatchInlineSnapshot('"Signer implementation expected."');
-        }
-    });
     it('expects PDF to contain a ByteRange placeholder', async () => {
         try {
             await signpdf.sign(Buffer.from('No BR placeholder\n%%EOF'), new Signer());
